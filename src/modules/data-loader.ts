6dc7da01b77f08a26d9800c2fd64289a8fde183f
enpitsulin <enpitsulin@gmail.com>
2025-10-31 21:41:35 +0800

    refactor: remove data loader

--- src/modules/data-loader.ts
+++
@@ -1,6 +0,0 @@
-import type { UserModule } from '~~/lib/types/app'
-import { DataLoaderPlugin } from 'unplugin-vue-router/data-loaders'
-
-export const install: UserModule = ({ app, router }) => {
-  app.use(DataLoaderPlugin, { router })
-}
