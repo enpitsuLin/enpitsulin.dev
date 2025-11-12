8b7b4440d0b16d1ac23d460077a3e8b5017a1c47
enpitsulin <enpitsulin@gmail.com>
2025-11-12 22:14:03 +0800

    chore: move to catalogs

--- workers/blog/astro.config.mjs
+++ workers/blog/astro.config.mjs
@@ -3,13 +3,13 @@
 import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), unocss()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
