a3afe7abf44eeae4c439ec1035c206137db279c8
enpitsulin <enpitsulin@gmail.com>
2025-11-12 22:27:10 +0800

    chore: add vue integration

--- workers/blog/astro.config.mjs
+++ workers/blog/astro.config.mjs
@@ -3,13 +3,14 @@
 import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import vue from '@astrojs/vue'
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), unocss()],
+  integrations: [mdx(), sitemap(), unocss(), vue()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
