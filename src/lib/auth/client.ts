6cf2c645fd248c78937324d59ec8fd01a5dd831d
enpitsulin <enpitsulin@gmail.com>
2025-11-13 14:38:01 +0800

    feat: auth client plugins

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,4 +1,11 @@
-import { createAuthClient } from 'better-auth/react' // make sure to import from better-auth/react
+import { passkeyClient } from '@better-auth/passkey/client'
+import { adminClient, multiSessionClient } from 'better-auth/client/plugins'
+import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
+  plugins: [
+    adminClient(),
+    passkeyClient(),
+    multiSessionClient(),
+  ],
 })
