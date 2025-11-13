02227224387cfa47173fb42616a7d44f29be8cb1
enpitsulin <enpitsulin@gmail.com>
2025-11-13 22:19:24 +0800

    feat: add vite-plugin-devtools-json

--- waku.config.ts
+++ waku.config.ts
@@ -1,6 +1,7 @@
 import nodeLoaderCloudflare from '@hiogawa/node-loader-cloudflare/vite'
 import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 import inspect from 'vite-plugin-inspect'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'waku/config'
@@ -8,6 +9,7 @@ import { defineConfig } from 'waku/config'
 export default defineConfig({
   vite: {
     plugins: [
+      devtoolsJson(),
       viteTsconfigPaths(),
       inspect(),
       unocss(),
