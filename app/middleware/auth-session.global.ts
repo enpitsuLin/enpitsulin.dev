8a7c7e8e9e2cb672760abcab8704fcbd5ab238ab
enpitsulin <enpitsulin@gmail.com>
2025-11-29 20:54:16 +0800

    feat: add route meta requiresAuth

--- app/middleware/auth-session.global.ts
+++ app/middleware/auth-session.global.ts
@@ -1,9 +1,14 @@
+import type { RouteMeta } from 'vue-router'
 import { defineNuxtRouteMiddleware } from '#app'
+import { defu } from 'defu'
 import { useAuthSession } from '~/composables/auth'
 
 export default defineNuxtRouteMiddleware(async (to) => {
+  const meta = defu<RouteMeta, [Required<Pick<RouteMeta, 'requireAuth'>>]>(to.meta, {
+    requireAuth: true,
+  })
   // If auth is disabled, skip middleware
-  if (to.meta?.auth === false) {
+  if (!meta.requiresAuth) {
     return
   }
 
