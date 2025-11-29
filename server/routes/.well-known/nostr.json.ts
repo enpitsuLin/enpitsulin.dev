dcc586353e4c96af7978fd487f65d1edb0ae31a8
enpitsulin <enpitsulin@gmail.com>
2025-11-29 18:36:41 +0800

    feat: add routes

---
+++ server/routes/.well-known/nostr.json.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler((event) => {
+  setHeaders(event, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Upgrade, Accept, Content-Type, User-Agent',
+  })
+
+  return {
+    names: {
+      me: '0aadfcac7327642509ec22ecb041d2e5257cda66a4565eb43114639bfe9d2ff0',
+    },
+    relays: {
+    },
+  }
+})
