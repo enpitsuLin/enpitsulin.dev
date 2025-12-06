253007363bd8f0e3e07578e060252ea21cb26302
enpitsulin <enpitsulin@gmail.com>
2025-12-06 16:58:19 +0800

    feat: list state styling

--- app/components/modules/list-empty.vue
+++ app/components/modules/list-empty.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { normalizeClass } from 'vue'
+
 const { type } = defineProps<{
   type: 'posts' | 'thoughts'
 }>()
@@ -21,33 +23,43 @@ const hint = type === 'posts' ? '（好灵感总是在洗澡时才出现 🚿）
       <div
         rounded-full
         absolute inset-0
-        class="bg-gradient-to-tr from-zinc-700 to-zinc-500 blur-2xl op-10 group-hover:op-20 transition-opacity duration-500"
+        class="bg-gradient-to-tr from-zinc-200 to-zinc-400 dark:from-zinc-700 dark:to-zinc-500 blur-2xl op-10 group-hover:op-20 transition-opacity duration-500"
       />
       <div
         relative p-6
-        border="~ zinc-800/50 rounded-full"
-        class="bg-zinc-900/50 ring-1 ring-zinc-700/20 shadow-2xl"
+        border="~ zinc-100/50 dark:zinc-800/50 rounded-full"
+        ring="1 zinc-200/20 dark:zinc-700/20"
+        bg="zinc-50/50 dark:zinc-900/50"
+        shadow-2xl
       >
         <div
           size-10
-          :class="[icon, type === 'posts' ? 'text-zinc-500 group-hover:text-zinc-300 ' : 'text-amber-500/80 group-hover:text-amber-400']"
+          :un-text="normalizeClass({
+            'zinc-500 group-hover:zinc-300': type === 'posts',
+            'amber-500/80 group-hover:amber-400': type === 'thoughts',
+          })"
+          :class="[icon]"
           class="transition-colors duration-300"
         />
 
         <div
           absolute
-          border="~ zinc-700 rounded-full"
-          class="-top-1 -right-1 bg-zinc-800 p-1.5 animate-bounce delay-75"
+          border="~ zinc-200 dark:zinc-700 rounded-full"
+          bg="zinc-100 dark:zinc-800"
+          class="-top-1 -right-1  p-1.5 animate-bounce delay-75"
         >
-          <div :class="tipIcon" size-3 text-zinc-400 />
+          <div :class="tipIcon" size-3 un-text="zinc-500 dark:zinc-400" />
         </div>
       </div>
     </div>
 
-    <h3 text-xl font-medium text-zinc-200 mb-2>
+    <h3 text-xl font-medium un-text="zinc-800 dark:zinc-200" mb-2>
       {{ title }}
     </h3>
-    <p text-zinc-500 max-w-sm mx-auto mb-8 text-sm leading-relaxed>
+    <p
+      un-text="zinc-400 dark:zinc-500"
+      max-w-sm mx-auto mb-8 text-sm leading-relaxed
+    >
       {{ description }}
       <br>
       <span text-xs op-70>{{ hint }}</span>
