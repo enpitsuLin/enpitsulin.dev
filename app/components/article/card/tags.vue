1227c2271152e74c8be8d0c967486b860dad11d0
enpitsulin <enpitsulin@gmail.com>
2025-12-08 20:04:06 +0800

    fix: update tag button styles for improved visibility

--- app/components/article/card/tags.vue
+++ app/components/article/card/tags.vue
@@ -10,7 +10,7 @@ defineProps<{ tags: string[] }>()
       :to="{ name: 'blog-tag-tag-page', params: { tag } }"
       border="~ rounded zinc-200/50 dark:zinc-700/50"
       un-text="xs zinc-600 dark:zinc-500"
-      bg="zinc-100 dark:zinc-800 op-50 hover:op-60"
+      bg="zinc-100/50 dark:zinc-800/50 hover:zinc-200/50 dark:hover:zinc-700/50"
       class="px-2 py-0.5"
     >
       # {{ tag }}
