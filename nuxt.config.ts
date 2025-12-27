f34c07eee05dc4753976c7caf4c3d5902fa16d1b
enpitsulin <enpitsulin@gmail.com>
2025-12-27 16:25:28 +0800

    build: fix build issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -113,6 +113,9 @@ export default defineNuxtConfig({
     experimental: {
       websocket: true,
     },
+    unenv: {
+      external: ['cloudflare:workers'],
+    },
     // minify: false,
   },
 
