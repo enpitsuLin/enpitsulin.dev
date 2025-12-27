df0453085e9e8e0bafee6dab561f2d90e9fac384
enpitsulin <enpitsulin@gmail.com>
2025-12-27 21:45:43 +0800

    chore: remove auth

--- app/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to) => {
-  const { loggedIn, user } = useUserSession()
-  if (!loggedIn.value || user.value?.role !== 'admin') {
-    return navigateTo({
-      path: '/admin/sign-in',
-      query: { redirect: to.path },
-    })
-  }
-})
